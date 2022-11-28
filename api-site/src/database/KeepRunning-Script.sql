83b8241dabf70b720abac40c8dea3d045a05c1e7
glvPedro <pedro.gsilva@sptech.school>
2022-11-28 11:31:40 -0300

    versinalemnto vFinal

--- api-site/src/database/KeepRunning-Script.sql
+++ api-site/src/database/KeepRunning-Script.sql
@@ -9,6 +9,8 @@ dtNascimento date,
 senha varchar(45)
 );
 
+insert into usuario (nome, email, dtNascimento, senha) values ("fernanda",'fernanda@gmail.com','1996-04-22','1234');
+
 create table treino(
 id int,
 duracao time,
@@ -19,10 +21,6 @@ fk_usuario int,
 foreign key (fk_usuario) references usuario(id)
 );
 
-select * from usuario;
-select * from treino;
-
-insert into usuario (nome, email, dtNascimento, senha) values ("fernanda",'fernanda@gmail.com','1996-04-22','1234');
 insert into treino values (1,'00:21:02','1.12','5.13',613,1),
 						  (2,'01:43:21','2.23','4.23',345,1),
                           (3,'00:21:23','1.43','2.43',423,1),
@@ -30,5 +28,6 @@ insert into treino values (1,'00:21:02','1.12','5.13',613,1),
                           (5,'00:53:34','3.76','1.24',342,1),
                           (6,'00:52:45','2.34','3.54',251,1);
 
-select u.nome, t. duracao duração, t.distancia distância, t.ritmo, t.calorias
-from usuario u join treino t on u.id = t.fk_usuario;
+select * from usuario;
+select * from treino;
+select * from usuario u join treino t on u.id = t.fk_usuario;
